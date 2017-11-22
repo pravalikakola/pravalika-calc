31186c35ab00c95b9bf0831e189300ce561cb0b9
pravalikakola <32535182+pravalikakola@users.noreply.github.com>
2017-11-22 01:21:10 -0500

    Create nd.js

---
+++ nd.js
@@ -0,0 +1,13 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+
+   
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   
+   response.end('good evening\n');
+}).listen(8081);
+
+
+console.log('Server running at http://127.0.0.1:8081/');
